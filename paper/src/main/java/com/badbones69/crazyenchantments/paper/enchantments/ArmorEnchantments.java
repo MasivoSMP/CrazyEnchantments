b01ebd95c3ace7a1e07fee0d94597d68cf967f74
Fideodeo <81789969+Fideo456@users.noreply.github.com>
2026-01-17 19:50:01 -0300

    Adjust Shockwave enchantment velocity parameters for improved gameplay balance

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/enchantments/ArmorEnchantments.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/enchantments/ArmorEnchantments.java
@@ -257,7 +257,7 @@ public class ArmorEnchantments implements Listener {
             }
 
             if (EnchantUtils.isEventActive(CEnchantments.SHOCKWAVE, player, armor, enchants)) {
-                damager.setVelocity(player.getLocation().getDirection().multiply(2).setY(1.25));
+                damager.setVelocity(player.getLocation().getDirection().multiply(1.5).setY(1));
             }
 
             if (player.getHealth() <= event.getFinalDamage() && EnchantUtils.isEventActive(CEnchantments.SYSTEMREBOOT, player, armor, enchants)) {
